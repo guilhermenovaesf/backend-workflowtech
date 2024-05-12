a91cc9781c65cee6bd22ec55128add79fb9b4cc1
Guilherme Novaes <Guilhermenovaes3000@gmail.com>
2024-05-12 10:37:30 -0300

    :construction: initial commit

---
+++ src/main/java/com/example/gestao_fluxos_trabalho/GestaoFluxosTrabalhoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gestao_fluxos_trabalho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFluxosTrabalhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoFluxosTrabalhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gestao_fluxos_trabalho/GestaoFluxosTrabalhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestao_fluxos_trabalho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFluxosTrabalhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
